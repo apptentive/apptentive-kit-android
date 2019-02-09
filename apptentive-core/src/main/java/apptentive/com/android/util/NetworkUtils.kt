1469b896ba64060d2461830eddac14f464498aa7
Alex Lementuev <a.lementuev@gmail.com>
2019-02-08 17:16:31 -0800

    Refactoring

--- apptentive-core/src/main/java/apptentive/com/android/util/NetworkUtils.kt
+++ apptentive-core/src/main/java/apptentive/com/android/util/NetworkUtils.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 
+/**
+ * Utility class for network related queries.
+ */
 object NetworkUtils {
     /**
      * Indicates whether network connectivity exists or is in the process
@@ -11,8 +14,14 @@ object NetworkUtils {
      */
     fun isNetworkConnected(context: Context): Boolean {
         // TODO: check ACCESS_NETWORK_STATE permission
+        return getActiveNetwork(context)?.isConnectedOrConnecting == true
+    }
+
+    /**
+     * Returns active network (if present)
+     */
+    private fun getActiveNetwork(context: Context): NetworkInfo? {
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
-        return activeNetwork?.isConnectedOrConnecting == true
+        return cm.activeNetworkInfo
     }
 }
