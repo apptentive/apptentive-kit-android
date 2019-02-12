629361db6fba52b8f204149a2ec1fe749e63c707
Alex Lementuev <a.lementuev@gmail.com>
2019-02-11 16:58:40 -0800

    HttpClient test progress
    Added mock objects

--- apptentive-core/src/test/java/apptentive/com/android/network/HttpClientTest.kt
+++ apptentive-core/src/test/java/apptentive/com/android/network/HttpClientTest.kt
@@ -1,12 +1,59 @@
 package apptentive.com.android.network
 
-import org.junit.Test
+import apptentive.com.android.concurrent.ImmediateExecutionQueue
+import junit.framework.TestCase
+import org.junit.Ignore
 
-import org.junit.Assert.*
+class HttpClientImplTest : TestCase() {
+    private lateinit var network: MockHttpNetwork
+    private lateinit var networkQueue: ImmediateExecutionQueue
 
-class HttpClientTest {
-
-    @Test
-    fun send() {
+    override fun setUp() {
+        super.setUp()
+        network = MockHttpNetwork()
+        networkQueue = ImmediateExecutionQueue("network", dispatchManually = true)
     }
+
+    @Ignore("Implement me!")
+    fun testSend() {
+        val client = createHttpClient()
+        // TODO: send request
+    }
+
+    //region Helpers
+
+    private fun createHttpClient(retryPolicy: HttpRequestRetryPolicy? = null): HttpClient {
+        return HttpClientImpl(
+            network,
+            networkQueue,
+            retryPolicy ?: HttpRequestNoRetryPolicy
+        )
+    }
+
+    private fun dispatchRequests() {
+        networkQueue.dispatchAll()
+    }
+
+    //endregion
+}
+
+private class MockHttpRequest(
+    val mockResponse: HttpResponse,
+    method: HttpMethod = HttpMethod.GET
+) : HttpRequest(method, "https://example.com") {
+    override fun createRequestBody(): ByteArray? = mockResponse.content
+    override fun parseResponseBody(bytes: ByteArray) = Unit
+}
+
+private class MockHttpNetwork : HttpNetwork {
+    var networkConnected: Boolean = true
+
+    override val isNetworkConnected: Boolean get() = networkConnected
+
+    override fun performRequest(request: HttpRequest): HttpResponse = (request as MockHttpRequest).mockResponse
+}
+
+private object HttpRequestNoRetryPolicy : HttpRequestRetryPolicy {
+    override fun shouldRetry(request: HttpRequest) = false
+    override fun getRetryDelay(request: HttpRequest) = 0.0
 }
