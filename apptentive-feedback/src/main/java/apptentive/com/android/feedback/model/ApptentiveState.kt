6a5d03d8a4b5f6376d70288e14ea30c5ec4e22a8
Alex Lementuev <a.lementuev@gmail.com>
2019-03-07 13:29:38 -0800

    Removed comment

--- apptentive-feedback/src/main/java/apptentive/com/android/feedback/model/ApptentiveState.kt
+++ apptentive-feedback/src/main/java/apptentive/com/android/feedback/model/ApptentiveState.kt
@@ -3,7 +3,6 @@ package apptentive.com.android.feedback.model
 import apptentive.com.android.feedback.utils.randomUUID
 import org.rekotlin.StateType
 
-// FIXME: figure out a better name since this one might be confused with the host app state
 data class ApptentiveState(
     val activeConversation: Conversation
 ) : StateType {
