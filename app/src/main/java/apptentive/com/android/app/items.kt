a738e9d782301f95ceedfe7ed25131b3b4d41d0b
Alex Lementuev <a.lementuev@gmail.com>
2019-03-12 11:57:00 -0700

    Added more colors

--- app/src/main/java/apptentive/com/android/app/items.kt
+++ app/src/main/java/apptentive/com/android/app/items.kt
@@ -28,6 +28,30 @@ class BeverageItem(val id: String, val imageId: Int, val title: String) : Item(I
 class FeedbackItem(private val title: String) : Item(ItemType.FEEDBACK.ordinal) {
     class ViewHolder(view: View) : RecyclerViewAdapter.ViewHolder<FeedbackItem>(view) {
         private val titleView: TextView = view.findViewById(R.id.title)
+        private val buttons = arrayOf<ImageButton>(
+            view.findViewById(R.id.button_satisfied),
+            view.findViewById(R.id.button_neutral),
+            view.findViewById(R.id.button_dissatisfied)
+        )
+        private val colors = intArrayOf(
+            R.color.colorSatisfied,
+            R.color.colorNeutral,
+            R.color.colorDissatisfied)
+
+        init {
+            val colorFilter = buttons[0].colorFilter
+            for (i in 0 until buttons.size) {
+                buttons[i].setOnClickListener {
+                    for (j in 0 until buttons.size) {
+                        if (i == j) {
+                            buttons[j].setColorFilter(buttons[j].context.getColor(colors[j]))
+                        } else {
+                            buttons[j].colorFilter = colorFilter
+                        }
+                    }
+                }
+            }
+        }
 
         override fun bindView(item: FeedbackItem, position: Int) {
             titleView.text = item.title
@@ -45,6 +69,7 @@ class RatingItem(private val title: String) : Item(ItemType.RATING.ordinal) {
             view.findViewById(R.id.button_star_4),
             view.findViewById(R.id.button_star_5)
         )
+
         init {
             for (i in 0 until buttons.size) {
                 buttons[i].setOnClickListener { setRating(i + 1) }
@@ -67,16 +92,31 @@ class RatingItem(private val title: String) : Item(ItemType.RATING.ordinal) {
 class SurveyItem(private val question: String, private val answers: Array<String>) : Item(ItemType.SURVEY.ordinal) {
     class ViewHolder(view: View) : RecyclerViewAdapter.ViewHolder<SurveyItem>(view) {
         private val titleView: TextView = view.findViewById(R.id.question)
-        private val answerButtons = arrayOf<Button>(
+        private val buttons = arrayOf<Button>(
             view.findViewById(R.id.button_answer_1),
             view.findViewById(R.id.button_answer_2),
             view.findViewById(R.id.button_answer_3)
         )
 
+        init {
+            val colors = buttons[0].textColors
+            for (i in 0 until buttons.size) {
+                buttons[i].setOnClickListener {
+                    for (j in 0 until buttons.size) {
+                        if (i == j) {
+                            buttons[j].setTextColor(buttons[j].context.getColor(R.color.colorAccent))
+                        } else {
+                            buttons[j].setTextColor(colors)
+                        }
+                    }
+                }
+            }
+        }
+
         override fun bindView(item: SurveyItem, position: Int) {
             titleView.text = item.question
             for (i in 0 until item.answers.size) {
-                answerButtons[i].text = item.answers[i]
+                buttons[i].text = item.answers[i]
             }
         }
     }
