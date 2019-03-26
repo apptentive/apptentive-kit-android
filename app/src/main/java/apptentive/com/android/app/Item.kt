5bc9b1b61a3d9421a9642f0401b258e9a705117a
Alex Lementuev <a.lementuev@gmail.com>
2019-03-26 13:03:53 -0700

    Cleaned up sample app

--- app/src/main/java/apptentive/com/android/app/Item.kt
+++
@@ -1,5 +0,0 @@
-package apptentive.com.android.app
-
-open class Item(internal val itemViewType: Int = 0) {
-    val itemId: Long get() = 0
-}
--- app/src/main/java/apptentive/com/android/app/MyApplication.kt
+++ app/src/main/java/apptentive/com/android/app/MyApplication.kt
@@ -1,16 +1,14 @@
 package apptentive.com.android.app
 
 import android.app.Application
-import apptentive.com.android.feedback.Apptentive
-import apptentive.com.android.feedback.ApptentiveConfiguration
+import apptentive.com.android.love.ApptentiveLove
 
 class MyApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
-        val apptentiveKey = "ANDROID-ANDROID-DEV-c9c0b324114f"
-        val apptentiveSignature = "98f5539e9310dc290394c68b76664e98"
-        val configuration = ApptentiveConfiguration(apptentiveKey, apptentiveSignature)
-        Apptentive.register(this, configuration)
+        val apptentiveKey = "apptentive_key"
+        val apptentiveSignature = "apptentive_signature"
+        ApptentiveLove.register(this, apptentiveKey, apptentiveSignature)
     }
 }
