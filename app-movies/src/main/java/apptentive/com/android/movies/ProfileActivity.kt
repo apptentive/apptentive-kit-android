1dbf7dcdb1e48ba12957a9101d82523008819769
Alex Lementuev <a.lementuev@gmail.com>
2019-04-01 17:04:54 -0700

    Profile progress

--- app-movies/src/main/java/apptentive/com/android/movies/ProfileActivity.kt
+++ app-movies/src/main/java/apptentive/com/android/movies/ProfileActivity.kt
@@ -2,12 +2,16 @@ package apptentive.com.android.movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var viewModel: ProfileViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
@@ -19,10 +23,30 @@ class ProfileActivity : AppCompatActivity() {
         }
 
         val factory = ViewModelFactory.getInstance(this)
-        val viewModel = ViewModelProviders.of(this, factory).get(ProfileViewModel::class.java)
+        viewModel = ViewModelProviders.of(this, factory).get(ProfileViewModel::class.java)
 
-        setAutocomplete(username, viewModel.usernames)
-        setAutocomplete(email, viewModel.emails)
+        usernameEditText.setText(viewModel.person.name ?: "")
+        emailEditText.setText(viewModel.person.email ?: "")
+
+        setAutocomplete(usernameEditText, viewModel.usernames)
+        setAutocomplete(emailEditText, viewModel.emails)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_profile, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.buttonSave) {
+            val username = usernameEditText.text.toString().trim()
+            val email = emailEditText.text.toString().trim()
+            viewModel.person.name = username
+            viewModel.person.email = email
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     private fun setAutocomplete(textView: AutoCompleteTextView, suggestions: Array<String>) {
