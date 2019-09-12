d29cccfa729185b927dccbcfa0ad826596a31d5d
Alex Lementuev <a.lementuev@gmail.com>
2019-09-12 11:59:17 -0700

    Serialization progress

--- apptentive-feedback/src/main/java/apptentive/com/android/feedback/model/Device.kt
+++ apptentive-feedback/src/main/java/apptentive/com/android/feedback/model/Device.kt
@@ -1,4 +1,34 @@
 package apptentive.com.android.feedback.model
 
-class Device {
+data class Device(
+    val osName: String,
+    val osVersion: String,
+    val osBuild: String,
+    val osApiLevel: Int,
+    val manufacturer: String,
+    val model: String,
+    val board: String,
+    val product: String,
+    val brand: String,
+    val cpu: String,
+    val device: String,
+    val uuid: String,
+    val buildType: String,
+    val buildId: String
+) {
+    private var carrier: String? = null
+    private var currentCarrier: String? = null
+    private var networkType: String? = null
+
+    private var bootloaderVersion: String? = null
+    private var radioVersion: String? = null
+
+    private var localeCountryCode: String? = null
+    private var localeLanguageCode: String? = null
+    private var localeRaw: String? = null
+    private var utcOffset: String? = null
+    private var advertiserId: String? = null
+
+    val customData = CustomData()
+    val integrationConfig = IntegrationConfig()
 }
