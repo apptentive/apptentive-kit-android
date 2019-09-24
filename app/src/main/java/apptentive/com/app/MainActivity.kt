bf5f00e6500d61a4b4c7f0123d997f7f14148fe8
Alex Lementuev <a.lementuev@gmail.com>
2019-09-24 14:15:56 -0400

    Sample app progress

--- app/src/main/java/apptentive/com/app/MainActivity.kt
+++ app/src/main/java/apptentive/com/app/MainActivity.kt
@@ -1,66 +1,23 @@
 package apptentive.com.app
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import apptentive.com.android.concurrent.ExecutorQueue
-import apptentive.com.android.feedback.backend.ConversationTokenFetchBody
-import apptentive.com.android.feedback.backend.ConversationTokenFetchResponse
-import apptentive.com.android.feedback.backend.DefaultConversationService
-import apptentive.com.android.feedback.platform.DefaultAppReleaseFactory
-import apptentive.com.android.feedback.platform.DefaultDeviceFactory
-import apptentive.com.android.feedback.platform.DefaultSDKFactory
-import apptentive.com.android.network.DefaultHttpClient
-import apptentive.com.android.network.DefaultHttpNetwork
-import apptentive.com.android.network.DefaultHttpRequestRetryPolicy
-import apptentive.com.android.util.Callback
+import apptentive.com.android.feedback.Apptentive
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val SDK_VERSION = "5.4.6"
-        val APPTENTIVE_KEY_DEV = "ANDROID-ANDROID-DEV-c9c0b324114f"
-        val APPTENTIVE_SIGNATURE_DEV = "98f5539e9310dc290394c68b76664e98"
-        val CONFIG_DEFAULT_SERVER_URL = "https://api.apptentive.com"
-
-        val network = DefaultHttpNetwork(this)
-        val queue = ExecutorQueue.createConcurrentQueue(name = "network");
-        val client = DefaultHttpClient(
-            network = network,
-            networkQueue = queue,
-            retryPolicy = DefaultHttpRequestRetryPolicy()
-        )
-        val service = DefaultConversationService(
-            httpClient = client,
-            apptentiveKey = APPTENTIVE_KEY_DEV,
-            apptentiveSignature = APPTENTIVE_SIGNATURE_DEV,
-            apiVersion = 9,
-            sdkVersion = SDK_VERSION,
-            baseURL = CONFIG_DEFAULT_SERVER_URL,
-            callbackExecutor = ExecutorQueue.mainQueue
-        )
-
-        val device = DefaultDeviceFactory(this).create()
-        val sdk = DefaultSDKFactory(
-            version = SDK_VERSION,
-            distribution = "Default",
-            distributionVersion = SDK_VERSION
-        ).create()
-        val appRelease = DefaultAppReleaseFactory(this).create()
-        val payload = ConversationTokenFetchBody.from(
-            device = device,
-            sdk = sdk,
-            appRelease = appRelease
-        )
-        service.fetchConversationToken(payload, object : Callback<ConversationTokenFetchResponse> {
-            override fun onComplete(t: ConversationTokenFetchResponse) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        engage_button.setOnClickListener {
+            val eventName = event_name_edit_text.text.toString().trim()
+            if (eventName.isEmpty()) {
+                Toast.makeText(this, "Empty event name", Toast.LENGTH_LONG).show()
+            } else {
+                Apptentive.engage(this, eventName)
             }
-
-            override fun onFailure(t: Throwable) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-        })
+        }
     }
 }
--- app/src/main/java/apptentive/com/app/MyApplication.kt
+++ app/src/main/java/apptentive/com/app/MyApplication.kt
@@ -1,12 +1,17 @@
 package apptentive.com.app
 
 import android.app.Application
-import apptentive.com.android.core.DependencyProvider
+import apptentive.com.android.feedback.Apptentive
+import apptentive.com.android.feedback.ApptentiveConfiguration
 
-class MyApplication: Application() {
+class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        DependencyProvider.register(this)
+        val configuration = ApptentiveConfiguration(
+            apptentiveKey = "ANDROID-ANDROID-DEV-c9c0b324114f",
+            apptentiveSignature = "98f5539e9310dc290394c68b76664e98"
+        )
+        Apptentive.register(this, configuration)
     }
 }
