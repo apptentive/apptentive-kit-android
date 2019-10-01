0da4e72114d4f59f12fc40fb2a24e2ca240e9f7d
Alex Lementuev <a.lementuev@gmail.com>
2019-10-01 15:20:56 -0700

    Added additional function to work with time

--- apptentive-core/src/main/java/apptentive/com/android/core/typeAliases.kt
+++ apptentive-core/src/main/java/apptentive/com/android/core/typeAliases.kt
@@ -4,5 +4,7 @@ typealias TimeInterval = Double
 
 const val UNDEFINED: Int = -1
 
-fun toMilliseconds(time: TimeInterval) : Int = (time * 1000L).toInt()
-fun toSeconds(time: Long) : TimeInterval = time * 0.001
+fun toMilliseconds(time: TimeInterval): Int = (time * 1000L).toInt()
+fun toSeconds(time: Long): TimeInterval = time * 0.001
+
+fun getTimeSeconds(): TimeInterval = toSeconds(System.currentTimeMillis())
