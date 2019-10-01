9dfa63228044284d4abb761de8cd41017350fbed
Alex Lementuev <a.lementuev@gmail.com>
2019-10-01 15:52:48 -0700

    Properly release mocks on GenerateUUIDRule

--- apptentive-feedback/src/test/java/apptentive/com/android/feedback/GenerateUUIDRule.kt
+++ apptentive-feedback/src/test/java/apptentive/com/android/feedback/GenerateUUIDRule.kt
@@ -1,18 +1,24 @@
 package apptentive.com.android.feedback
 
 import apptentive.com.android.util.generateUUID
+import io.mockk.clearStaticMockk
 import io.mockk.every
 import io.mockk.mockkStatic
+import io.mockk.unmockkStatic
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 class GenerateUUIDRule : TestWatcher() {
     override fun starting(description: Description?) {
-        mockkStatic("apptentive.com.android.util.UUIDUtils")
+        mockkStatic(UTIL_CLASS)
         every { generateUUID() } returns "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
     }
 
     override fun finished(description: Description?) {
-        // TODO: clear mocks
+        unmockkStatic(UTIL_CLASS)
+    }
+
+    companion object {
+        private const val UTIL_CLASS = "apptentive.com.android.util.UUIDUtils"
     }
 }
