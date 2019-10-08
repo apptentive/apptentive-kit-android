d7365d396bbdf4247639c73daecade3fb4f65b06
Alex Lementuev <a.lementuev@gmail.com>
2019-10-08 12:29:10 -0700

    Renamed more fields

--- apptentive-feedback/src/main/java/apptentive/com/android/feedback/payload/SDKPayload.kt
+++ apptentive-feedback/src/main/java/apptentive/com/android/feedback/payload/SDKPayload.kt
@@ -2,12 +2,12 @@ package apptentive.com.android.feedback.payload
 
 class SDKPayload(
     nonce: String,
-    val author_email: String,
-    val author_name: String,
+    val authorEmail: String,
+    val authorName: String,
     val distribution: String,
-    val distribution_version: String,
+    val distributionVersion: String,
     val platform: String,
-    val programming_language: String,
+    val programmingLanguage: String,
     val version: String
 ) : Payload(nonce) {
 }
