e308f71101c336af29101c58da806645dd11303e
Alex Lementuev <a.lementuev@gmail.com>
2019-10-28 12:07:42 -0700

    Refactoring
    Removed unused code

--- apptentive-feedback/src/main/java/apptentive/com/android/feedback/engagement/criteria/InteractionRepository.kt
+++
@@ -1,9 +0,0 @@
-package apptentive.com.android.feedback.engagement.criteria
-
-import apptentive.com.android.feedback.engagement.Event
-import apptentive.com.android.feedback.engagement.interactions.InteractionData
-
-interface InteractionRepository {
-    fun getInteractionData(event: Event): InteractionData?
-}
-
