5701335ccde98cc53e83984e4526e69b8105d228
Alex Lementuev <a.lementuev@gmail.com>
2019-11-15 14:55:06 -0800

    Added empty test for persistent payload queue

--- apptentive-feedback/src/androidTest/java/PersistentPayloadQueueTest.kt
+++
@@ -1,9 +0,0 @@
-package apptentive.com.android.feedback.payload
-
-import org.junit.Assert.*
-
-class PersistentPayloadQueueTest {
-    // TODO: test enqueue
-    // TODO: test nextUnsent
-    // TODO: test remove
-}
---
+++ apptentive-feedback/src/androidTest/java/apptentive/com/android/feedback/payload/PersistentPayloadQueueTest.kt
@@ -0,0 +1,11 @@
+package apptentive.com.android.feedback.payload
+
+import org.junit.Ignore
+import org.junit.Test
+
+class PersistentPayloadQueueTest {
+    @Test
+    @Ignore
+    fun testEnqueueAndDequeue() {
+    }
+}
