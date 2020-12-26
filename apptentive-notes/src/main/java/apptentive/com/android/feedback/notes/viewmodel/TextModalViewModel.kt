9383f41c88cbc347b591faf19ab135d744a6a850
Alex Lementuev <a.lementuev@gmail.com>
2020-12-25 20:10:18 -0500

    TextModalViewModel progress

--- apptentive-notes/src/main/java/apptentive/com/android/feedback/notes/viewmodel/TextModalViewModel.kt
+++ apptentive-notes/src/main/java/apptentive/com/android/feedback/notes/viewmodel/TextModalViewModel.kt
@@ -4,6 +4,21 @@ import apptentive.com.android.feedback.notes.interaction.TextModalInteraction
 
 class TextModalViewModel(private val interaction: TextModalInteraction) {
     fun invokeAction(id: String) {
-        TODO("Not yet implemented")
+        val action = findAction(id) ?: throw IllegalArgumentException("Can't find action: $id")
+        when (action) {
+            is TextModalInteraction.Action.Dismiss -> {
+                // nothing
+            }
+            is TextModalInteraction.Action.Invoke -> {
+                TODO()
+            }
+            else -> {
+                throw IllegalArgumentException("Unexpected action: $action")
+            }
+        }
+    }
+
+    private fun findAction(id: String): TextModalInteraction.Action? {
+        return interaction.actions.find { it.id == id }
     }
 }
