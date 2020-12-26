0219dbf7a987ffd0fe4a569a9edbc9848d09c724
Alex Lementuev <a.lementuev@gmail.com>
2020-12-25 19:31:19 -0500

    TextModalViewModel + launcher progress

--- apptentive-notes/src/main/java/apptentive/com/android/feedback/notes/interaction/TextModalInteractionLauncher.kt
+++ apptentive-notes/src/main/java/apptentive/com/android/feedback/notes/interaction/TextModalInteractionLauncher.kt
@@ -1,5 +1,6 @@
 package apptentive.com.android.feedback.notes.interaction
 
+import apptentive.com.android.feedback.notes.viewmodel.TextModalViewModel
 import apptentive.com.android.feedback.platform.AndroidEngagementContext
 import apptentive.com.android.feedback.platform.AndroidInteractionLauncher
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -9,10 +10,25 @@ class TextModalInteractionLauncher : AndroidInteractionLauncher<TextModalInterac
         context: AndroidEngagementContext,
         interaction: TextModalInteraction
     ) {
+        val viewModel = TextModalViewModel(interaction)
         context.executors.main.execute {
             MaterialAlertDialogBuilder(context.androidContext).apply {
                 setTitle(interaction.title)
                 setMessage(interaction.body)
+                val actions = interaction.actions
+                setPositiveButton(actions[0].label) { _, _ ->
+                    viewModel.invokeAction(actions[0].id)
+                }
+                if (actions.size > 1) {
+                    setNegativeButton(actions[1].label) { _, _ ->
+                        viewModel.invokeAction(actions[1].id)
+                    }
+                }
+                if (actions.size > 2) {
+                    setNeutralButton(actions[2].label) { _, _ ->
+                        viewModel.invokeAction(actions[2].id)
+                    }
+                }
                 show()
             }
         }
---
+++ apptentive-notes/src/main/java/apptentive/com/android/feedback/notes/viewmodel/TextModalViewModel.kt
@@ -0,0 +1,9 @@
+package apptentive.com.android.feedback.notes.viewmodel
+
+import apptentive.com.android.feedback.notes.interaction.TextModalInteraction
+
+class TextModalViewModel(private val interaction: TextModalInteraction) {
+    fun invokeAction(id: String) {
+        TODO("Not yet implemented")
+    }
+}
