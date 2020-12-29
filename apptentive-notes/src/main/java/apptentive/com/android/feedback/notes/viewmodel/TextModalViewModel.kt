a1733896d15572f88b00bbb1095b975dd6bec77c
Alex Lementuev <a.lementuev@gmail.com>
2020-12-28 19:18:33 -0500

    Add missing internal events with TextModal interaction

--- apptentive-notes/src/main/java/apptentive/com/android/feedback/notes/viewmodel/TextModalViewModel.kt
+++ apptentive-notes/src/main/java/apptentive/com/android/feedback/notes/viewmodel/TextModalViewModel.kt
@@ -3,6 +3,7 @@ package apptentive.com.android.feedback.notes.viewmodel
 import apptentive.com.android.core.Callback
 import apptentive.com.android.feedback.EngagementResult
 import apptentive.com.android.feedback.engagement.EngagementContext
+import apptentive.com.android.feedback.engagement.Event
 import apptentive.com.android.feedback.notes.interaction.TextModalInteraction
 
 class TextModalViewModel(
@@ -13,14 +14,24 @@ class TextModalViewModel(
 
     fun invokeAction(id: String) {
         context.executors.state.execute {
-            val action = findAction(id) ?: throw IllegalArgumentException("Can't find action: $id")
-            when (action) {
+            val position = indexOfAction(id)
+            when (val action = interaction.actions[position]) {
                 is TextModalInteraction.Action.Dismiss -> {
-                    // nothing
+                    engageCodePoint(CODE_POINT_DISMISS)
                 }
                 is TextModalInteraction.Action.Invoke -> {
+                    // engage
+                    val data = mapOf<String, Any?>(
+                        "action_id" to action.id,
+                        "label" to action.label,
+                        "position" to position,
+                        "invoked_interaction_id" to null
+                    )
+                    engageCodePoint(CODE_POINT_INTERACTION, data)
+
+                    // run invocation
                     val result = context.engage(action.invocations)
-                    if (result != EngagementResult.Success) {
+                    if (result !is EngagementResult.Success) {
                         // FIXME: error message
                     }
                 }
@@ -29,7 +40,7 @@ class TextModalViewModel(
                         event = action.event,
                         interactionId = interaction.id
                     )
-                    if (result != EngagementResult.Success) {
+                    if (result !is EngagementResult.Success) {
                         // FIXME: error message
                     }
                 }
@@ -42,10 +53,30 @@ class TextModalViewModel(
     }
 
     fun onCancel() {
-        // TODO: engage 'cancel' event?
+        context.executors.state.execute {
+            engageCodePoint(CODE_POINT_CANCEL)
+        }
     }
 
-    private fun findAction(id: String): TextModalInteraction.Action? {
-        return interaction.actions.find { it.id == id }
+    private fun engageCodePoint(codePoint: String, data: Map<String, Any?>? = null) {
+        context.engage(
+            event = Event.internal(codePoint, interaction = "TextModal"),
+            interactionId = interaction.id,
+            data = data
+        )
+    }
+
+    private fun indexOfAction(id: String): Int {
+        val index = interaction.actions.indexOfFirst { it.id == id }
+        if (index == -1) {
+            throw IllegalArgumentException("Can't find action: $id")
+        }
+        return index
+    }
+
+    companion object {
+        const val CODE_POINT_INTERACTION = "interaction"
+        const val CODE_POINT_CANCEL = "cancel"
+        const val CODE_POINT_DISMISS = "dismiss"
     }
 }
