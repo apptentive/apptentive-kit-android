772e25c856e051e35db6299298fa914a46673b13
Alex Lementuev <a.lementuev@gmail.com>
2021-01-06 19:48:45 -0500

    Extracted TextModal dialog from the launcher

--- apptentive-notes/src/main/java/apptentive/com/android/feedback/notes/interaction/TextModalInteractionLauncher.kt
+++ apptentive-notes/src/main/java/apptentive/com/android/feedback/notes/interaction/TextModalInteractionLauncher.kt
@@ -1,13 +1,9 @@
 package apptentive.com.android.feedback.notes.interaction
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import android.widget.TextView
-import apptentive.com.android.feedback.notes.R
+import apptentive.com.android.feedback.notes.view.TextModalDialog
 import apptentive.com.android.feedback.notes.viewmodel.TextModalViewModel
 import apptentive.com.android.feedback.platform.AndroidEngagementContext
 import apptentive.com.android.feedback.platform.AndroidInteractionLauncher
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class TextModalInteractionLauncher : AndroidInteractionLauncher<TextModalInteraction>() {
     override fun launchInteraction(
@@ -19,34 +15,7 @@ class TextModalInteractionLauncher : AndroidInteractionLauncher<TextModalInterac
             interaction = interaction
         )
         context.executors.main.execute {
-            val dialog = MaterialAlertDialogBuilder(context.androidContext).apply {
-                val inflater = LayoutInflater.from(context.androidContext)
-                val contentView = inflater.inflate(R.layout.apptentive_note, null)
-                setView(contentView)
-
-                val titleView = contentView.findViewById<TextView>(R.id.alertTitle)
-                titleView.text = viewModel.title
-
-                val viewGroup = contentView.findViewById<ViewGroup>(R.id.apptentive_note_button_bar)
-                viewModel.actions.forEach { action ->
-                    val button = inflater.inflate(R.layout.apptentive_note_action, null) as TextView
-                    button.text = action.title
-                    viewGroup.addView(button)
-                    button.setOnClickListener {
-                        action.invoke()
-                    }
-                }
-
-                setOnCancelListener {
-                    viewModel.cancel()
-                }
-
-                viewModel.launch()
-            }.show()
-
-            viewModel.onDismiss = {
-                dialog.dismiss()
-            }
+            TextModalDialog(context.androidContext, viewModel).show()
         }
     }
 }
---
+++ apptentive-notes/src/main/java/apptentive/com/android/feedback/notes/view/TextModalDialog.kt
@@ -0,0 +1,53 @@
+package apptentive.com.android.feedback.notes.view
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import apptentive.com.android.feedback.notes.R
+import apptentive.com.android.feedback.notes.viewmodel.TextModalViewModel
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+class TextModalDialog(
+    context: Context,
+    private val viewModel: TextModalViewModel
+) {
+    private var dialog: AlertDialog
+
+    init {
+        dialog = MaterialAlertDialogBuilder(context).apply {
+            val inflater = LayoutInflater.from(context)
+            val contentView = inflater.inflate(R.layout.apptentive_note, null)
+            setView(contentView)
+
+            val titleView = contentView.findViewById<TextView>(R.id.alertTitle)
+            titleView.text = viewModel.title
+
+            val viewGroup = contentView.findViewById<ViewGroup>(R.id.apptentive_note_button_bar)
+            viewModel.actions.forEach { action ->
+                val button = inflater.inflate(R.layout.apptentive_note_action, null) as TextView
+                button.text = action.title
+                viewGroup.addView(button)
+                button.setOnClickListener {
+                    action.invoke()
+                }
+            }
+
+            setOnCancelListener {
+                viewModel.cancel()
+            }
+        }.create()
+
+        viewModel.onDismiss = {
+            dialog.dismiss()
+        }
+    }
+
+    fun show() {
+        require(!dialog.isShowing) { "Dialog already showing" }
+
+        dialog.show()
+        viewModel.launch()
+    }
+}
