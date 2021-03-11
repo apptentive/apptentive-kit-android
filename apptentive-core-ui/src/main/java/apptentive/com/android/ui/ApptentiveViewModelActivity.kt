8a31e5c5920997c92945a601cb0d107eac425eb7
Alex Lementuev <a.lementuev@gmail.com>
2021-03-10 20:03:14 -0500

    Fixed dark mode

--- apptentive-core-ui/src/main/java/apptentive/com/android/ui/ApptentiveViewModelActivity.kt
+++ apptentive-core-ui/src/main/java/apptentive/com/android/ui/ApptentiveViewModelActivity.kt
@@ -5,6 +5,9 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.annotation.CallSuper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_UNSPECIFIED
 
 open class ApptentiveViewModelActivity<VM : ApptentiveViewModel> : ApptentiveActivity() {
     // instance id must be passed with the intent when starting the activity and will be used to
@@ -21,9 +24,19 @@ open class ApptentiveViewModelActivity<VM : ApptentiveViewModel> : ApptentiveAct
     protected val viewModel: VM
         get() = _viewModel as VM
 
+    @CallSuper
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // set the local night mode to the same value as the parent context has
+        delegate.localNightMode = intent.getIntExtra(EXTRA_LOCAL_DARK_MODE, MODE_NIGHT_UNSPECIFIED)
+    }
+
     companion object {
         const val EXTRA_VIEW_MODEL_INSTANCE_ID =
             "apptentive.intent.extra.EXTRA_VIEW_MODEL_INSTANCE_ID"
+        const val EXTRA_LOCAL_DARK_MODE =
+            "apptentive.intent.extra.EXTRA_LOCAL_DARK_MODE"
     }
 }
 
@@ -36,7 +49,14 @@ inline fun <reified T : ApptentiveViewModelActivity<*>> Context.startViewModelAc
     if (extras != null) {
         intent.putExtras(extras)
     }
-    if (this !is Activity) {
+
+    // in case if the parent context is AppCompatActivity - we can check it's [localNightMode] flag
+    // and pass it to our activity
+    if (this is AppCompatActivity) {
+        intent.putExtra(ApptentiveViewModelActivity.EXTRA_LOCAL_DARK_MODE, delegate.localNightMode)
+    }
+    // if the event was engaged with the Application context, then start activity as a new task
+    else if (this !is Activity) {
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
     }
     startActivity(intent)
