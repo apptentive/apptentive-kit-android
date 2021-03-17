b3fe674353682a08debb6cff5668389dac9941c0
Twinkle Sharma <twinkle.sharma@apptentive.com>
2021-03-17 14:24:33 -0700

    changed request time out to 60 seconds as per iOS

--- apptentive-core/src/main/java/apptentive/com/android/network/Constants.kt
+++ apptentive-core/src/main/java/apptentive/com/android/network/Constants.kt
@@ -3,7 +3,7 @@ package apptentive.com.android.network
 import apptentive.com.android.core.TimeInterval
 
 internal object Constants {
-    const val DEFAULT_REQUEST_TIMEOUT: TimeInterval = 45.0
+    const val DEFAULT_REQUEST_TIMEOUT: TimeInterval = 60.0
     const val DEFAULT_RETRY_DELAY: TimeInterval = 5.0
     const val DEFAULT_RETRY_MAX_COUNT = 5
 }
