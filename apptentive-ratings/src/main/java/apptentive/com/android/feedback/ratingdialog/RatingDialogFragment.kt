dca49cf08052a889c342971631a5903a75609a6e
ChaseApptentive <85038972+ChaseApptentive@users.noreply.github.com>
2022-03-09 15:57:19 -0600

    PBI-3027 Rating Dialog MIC (#229)
    * PBI-3009 Notes minor interface customization
    [PBI-3009]
    * Re-worked how Notes are done so I can customize the title and message separately
    * Got rid of ButtonBarLayout since it's useless now
    * Made it look even closer to a real dialog than it did before
    
    * * Add contextThemeWrapper for dialog to override themes
    * Add style override for title with message, title without message, and message
    
    * Add background customization
    
    * Add button customization
    
    * Change background color type to color instead of reference
    
    * Fix notes with no style crash
    
    * Update custom styles example
    
    * Rename survey submit button to match note naming style
    
    * Notes MIC update mostly copying how legacy does it
    
    * PBI-2440 Random sampling targeting (#226)
    
    * PBI-2950 Create object to store random sampling
    [PBI-2950]
    
    * PBI-2440 Random sampling targeting
    [PBI-2440]
    
    * ktlint
    
    * Add in debug boolean
    
    * ktlint
    
    * Update for debug
    
    * Better solution for number comparisons
    
    * Make lowest 0.0 (default when only a until is given)
    
    * Add debug BuildConfig to class
    
    * Convert all conditional operators to when conditions
    
    * Update random sampling with random/percent case
    
    * Add random test cases
    
    * Fix some tests
    
    * ktlint
    
    * LoveDialog minor customization
    
    * provider love dialog specific styles
    
    * Update sonatype and jfrog versions and removed unused style
    
    * Update to material textview
    
    * PBI-3027 Rating Dialog MIC
    [PBI-3027]
    
    * Add in style example
    
    * Rename title to title_with_message to be clearer
    
    * Use correct layout
    
    * Extract some dimens to dimens file
    
    * use no_message style for love dialog
    
    * Replaced LinearLayout with ConstraintLayout, added maxWidth for Button
    
    * removed ellipsis, added missing title style
    
    * Love dialog with spec close to default material alert dialog
    
    Co-authored-by: Poornima <poorniman@Poornimas-MacBook-Pro.local>

--- apptentive-ratings/src/main/java/apptentive/com/android/feedback/ratingdialog/RatingDialogFragment.kt
+++ apptentive-ratings/src/main/java/apptentive/com/android/feedback/ratingdialog/RatingDialogFragment.kt
@@ -1,47 +1,55 @@
 package apptentive.com.android.feedback.ratingdialog
 
+import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.TextView
-import androidx.appcompat.widget.DialogTitle
+import androidx.appcompat.view.ContextThemeWrapper
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import apptentive.com.android.feedback.ratings.R
+import apptentive.com.android.ui.overrideTheme
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textview.MaterialTextView
 
 internal class RatingDialogFragment : DialogFragment() {
 
     private val viewModel by viewModels<RatingDialogViewModel>()
 
+    @SuppressLint("UseGetLayoutInflater", "InflateParams")
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialog = MaterialAlertDialogBuilder(requireContext()).apply {
-            val inflater = LayoutInflater.from(context)
+
+            val contextWrapper = ContextThemeWrapper(requireContext(), R.style.Theme_Apptentive).apply {
+                overrideTheme()
+            }
+            val inflater = LayoutInflater.from(contextWrapper)
             val contentView = inflater.inflate(R.layout.apptentive_rating_dialog, null)
             setView(contentView)
 
-            val titleView = contentView.findViewById<DialogTitle>(R.id.apptentive_rate_title)
+            val titleView = contentView.findViewById<MaterialTextView>(R.id.apptentive_rate_title)
             titleView.text = viewModel.title.orEmpty()
 
-            val messageView = contentView.findViewById<TextView>(R.id.apptentive_rate_message)
+            val messageView = contentView.findViewById<MaterialTextView>(R.id.apptentive_rate_message)
             messageView.text = viewModel.message.orEmpty()
 
-            val rateButton = contentView.findViewById<TextView>(R.id.apptentive_rate_button)
+            val rateButton = contentView.findViewById<MaterialButton>(R.id.apptentive_rate_button)
             rateButton.text = viewModel.rateText.orEmpty()
             rateButton.setOnClickListener {
                 viewModel.onRateButton()
                 dismiss()
             }
 
-            val remindButton = contentView.findViewById<TextView>(R.id.apptentive_remind_button)
+            val remindButton = contentView.findViewById<MaterialButton>(R.id.apptentive_remind_button)
             remindButton.text = viewModel.remindText.orEmpty()
             remindButton.setOnClickListener {
                 viewModel.onRemindButton()
                 dismiss()
             }
 
-            val declineButton = contentView.findViewById<TextView>(R.id.apptentive_decline_button)
+            val declineButton = contentView.findViewById<MaterialButton>(R.id.apptentive_decline_button)
             declineButton.text = viewModel.declineText.orEmpty()
             declineButton.setOnClickListener {
                 viewModel.onDeclineButton()
