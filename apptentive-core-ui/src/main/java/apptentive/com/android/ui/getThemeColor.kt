87e3584ab58de93546afe9bf36828d9976902ad7
ChaseApptentive <85038972+ChaseApptentive@users.noreply.github.com>
2022-03-23 13:31:13 -0500

    PBI-3048 Update example app with MIC (#237)
    * PBI-3009 Notes minor interface customization
    [PBI-3009]
    * Re-worked how Notes are done so I can customize the title and message separately
    * Got rid of ButtonBarLayout since it's useless now
    * Made it look even closer to a real dialog than it did before
    
    * * Add contextThemeWrapper for dialog to override themes
    * Add style override for title with message, title without message, and message
    
    * Add background customization
    
    * Add button customization
    
    * Change background color type to color instead of reference
    
    * Fix notes with no style crash
    
    * Update custom styles example
    
    * Rename survey submit button to match note naming style
    
    * Notes MIC update mostly copying how legacy does it
    
    * PBI-2440 Random sampling targeting (#226)
    
    * PBI-2950 Create object to store random sampling
    [PBI-2950]
    
    * PBI-2440 Random sampling targeting
    [PBI-2440]
    
    * ktlint
    
    * Add in debug boolean
    
    * ktlint
    
    * Update for debug
    
    * Better solution for number comparisons
    
    * Make lowest 0.0 (default when only a until is given)
    
    * Add debug BuildConfig to class
    
    * Convert all conditional operators to when conditions
    
    * Update random sampling with random/percent case
    
    * Add random test cases
    
    * Fix some tests
    
    * ktlint
    
    * LoveDialog minor customization
    
    * provider love dialog specific styles
    
    * Update sonatype and jfrog versions and removed unused style
    
    * Update to material textview
    
    * PBI-3027 Rating Dialog MIC
    [PBI-3027]
    
    * Add in style example
    
    * Rename title to title_with_message to be clearer
    
    * Use correct layout
    
    * Extract some dimens to dimens file
    
    * use no_message style for love dialog
    
    * Replaced LinearLayout with ConstraintLayout, added maxWidth for Button
    
    * removed ellipsis, added missing title style
    
    * [PBI-3010]
    
    * Love dialog with spec close to default material alert dialog
    
    * Add survey introduction MIC and cleanup
    
    * Survey Toolbar MIC
    
    * Update button footer and name consistency
    
    * Update footer MIC
    
    * cleanup
    
    * Survey Bottom app bar MIC
    
    * Cleanup
    
    * Update styling with colors of widgets
    
    * Fix text box hint color
    
    * Fix text box hint color
    
    * Cleanup styles
    
    * Clean up attrs
    
    * Cleanup
    
    * Update survey for terms and conditions
    
    * Cleanup styles and add examples
    
    * Update naming / cleanup
    
    * Remove import
    
    * Fix styles
    
    * Fix button background color
    
    * Add button text color
    
    * Removed some excessive styles
    
    * Remove unused style
    
    * PBI-3050 Survey Image Header Alternate
    [PBI-3050]
    Alternate approach using styles
    
    * Fix margin resource
    
    * Add resources
    
    * Add missing resources
    
    * Fix resource and config kdocs
    
    * Remove a couple extra internal style items
    
    * Use android:colorbackground not android:background
    
    * PBI-3048 Update example app with MIC
    [PBI-3048]
    
    * Update examples with alphas and message or text only styles
    
    * Update example app themes
    
    * Update alphas and colors a little with design help
    
    * Have checkbox radio button use colorPrimary
    
    * Add annotation to function
    
    * Add note about alpha affects
    
    * Update dark mode error colors
    
    Co-authored-by: Poornima <poorniman@Poornimas-MacBook-Pro.local>

--- apptentive-core-ui/src/main/java/apptentive/com/android/ui/getThemeColor.kt
+++ apptentive-core-ui/src/main/java/apptentive/com/android/ui/getThemeColor.kt
@@ -2,9 +2,10 @@ package apptentive.com.android.ui
 
 import android.content.Context
 import android.util.TypedValue
+import androidx.annotation.AttrRes
 
 
-fun Context.getThemeColor(attr: Int) : Int{
+fun Context.getThemeColor(@AttrRes attr: Int): Int {
     val typedValue = TypedValue()
     theme.resolveAttribute(attr, typedValue, true)
     return typedValue.data
