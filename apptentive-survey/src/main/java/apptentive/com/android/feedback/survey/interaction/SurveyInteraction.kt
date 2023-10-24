01fde9c18929e7f0ca3b5c79776a15e49ced2f8d
Luqmaan <luq.j.khan@gmail.com>
2023-10-24 16:41:42 -0500

    remove elf is null

--- apptentive-survey/src/main/java/apptentive/com/android/feedback/survey/interaction/SurveyInteraction.kt
+++ apptentive-survey/src/main/java/apptentive/com/android/feedback/survey/interaction/SurveyInteraction.kt
@@ -57,7 +57,7 @@ internal class SurveyInteraction(
         val link: String?
     ) {
         fun convertToLink(): Spanned {
-            val httpsLink = link?.let { if (it.startsWith("https://", true) || it.startsWith("http://", true)) it else "https://$it" } ?: ""
+            val httpsLink = if (link?.startsWith("https://", true) == true || link?.startsWith("http://", true) == true) link else "https://$link"
             val linkText = "<a href=$httpsLink>$label</a>"
             return HtmlCompat.fromHtml(linkText, HtmlCompat.FROM_HTML_MODE_COMPACT)
         }
