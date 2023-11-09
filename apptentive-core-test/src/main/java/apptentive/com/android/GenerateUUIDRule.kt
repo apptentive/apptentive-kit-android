74aba39d4adf3b1dfef5e93141d793f809f2aa1f
Frank Schmitt <frank.schmitt@alchemer.com>
2023-11-09 11:42:04 -0800

    Mock session ID generation

--- apptentive-core-test/src/main/java/apptentive/com/android/GenerateUUIDRule.kt
+++ apptentive-core-test/src/main/java/apptentive/com/android/GenerateUUIDRule.kt
@@ -1,8 +1,12 @@
 package apptentive.com.android
 
+import apptentive.com.android.feedback.ApptentiveDefaultClient
+import apptentive.com.android.feedback.ApptentiveDefaultClient.Companion.getSessionId
 import apptentive.com.android.util.generateUUID
 import io.mockk.every
+import io.mockk.mockkObject
 import io.mockk.mockkStatic
+import io.mockk.unmockkObject
 import io.mockk.unmockkStatic
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
@@ -11,10 +15,17 @@ class GenerateUUIDRule : TestWatcher() {
     override fun starting(description: Description) {
         mockkStatic(UTIL_CLASS)
         every { generateUUID() } returns "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+
+        mockkObject(ApptentiveDefaultClient) {
+            every {
+                getSessionId()
+            } returns "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+        }
     }
 
     override fun finished(description: Description) {
         unmockkStatic(UTIL_CLASS)
+        unmockkObject(ApptentiveDefaultClient)
     }
 
     companion object {
