c9046a11953664854e683f6bb7ebf12f232c7cac
PoornimaApptentive <85186738+PoornimaApptentive@users.noreply.github.com>
2024-03-20 09:12:00 -0700

    PBI-6314 single line button (#646)
    * restricted prompts label length to single line without ellipsis
    
    * bump up the api version to 13
    
    [PBI-6314]

--- apptentive-feedback/src/main/java/apptentive/com/android/feedback/Constants.kt
+++ apptentive-feedback/src/main/java/apptentive/com/android/feedback/Constants.kt
@@ -5,7 +5,7 @@ import apptentive.com.android.util.InternalUseOnly
 @InternalUseOnly
 object Constants {
     const val SDK_VERSION = "6.5.1"
-    const val API_VERSION = 12
+    const val API_VERSION = 13
     const val SERVER_URL = "https://api.apptentive.com"
     const val REDACTED_DATA = "<REDACTED>"
     private const val CONVERSATION_PATH = "/conversations/:conversation_id/"
