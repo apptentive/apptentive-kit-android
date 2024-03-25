f319f90ceccd2fee0303caaedaa82fe513fc3d51
PoornimaApptentive <85186738+PoornimaApptentive@users.noreply.github.com>
2024-03-25 12:26:14 -0700

    [PBI-6349] Release PR (#649)
    * release PR
    
    * Update Constants.kt

--- apptentive-feedback/src/main/java/apptentive/com/android/feedback/Constants.kt
+++ apptentive-feedback/src/main/java/apptentive/com/android/feedback/Constants.kt
@@ -4,7 +4,7 @@ import apptentive.com.android.util.InternalUseOnly
 
 @InternalUseOnly
 object Constants {
-    const val SDK_VERSION = "6.5.1"
+    const val SDK_VERSION = "6.6.0"
     const val API_VERSION = 13
     const val SERVER_URL = "https://api.apptentive.com"
     const val REDACTED_DATA = "<REDACTED>"
