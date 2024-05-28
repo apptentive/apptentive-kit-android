4abd4ec6459294804378659a333dd6fbc5f85585
PoornimaApptentive <85186738+PoornimaApptentive@users.noreply.github.com>
2024-05-28 10:50:02 -0700

    PBI-6783 Webview state fix (#662)
    * save the webview state and restore it with the rotation

--- apptentive-navigate-to-link/src/main/java/apptentive/com/android/feedback/link/view/NavigateTolinkActivity.kt
+++ apptentive-navigate-to-link/src/main/java/apptentive/com/android/feedback/link/view/NavigateTolinkActivity.kt
@@ -1,13 +1,13 @@
 package apptentive.com.android.feedback.link.view
 
 import android.os.Bundle
-import android.webkit.WebSettings
 import android.webkit.WebView
 import apptentive.com.android.feedback.link.R
 import apptentive.com.android.ui.hideSoftKeyboard
 import com.google.android.material.appbar.MaterialToolbar
 
 internal class NavigateTolinkActivity : BaseNavigateToLinkActivity() {
+    private lateinit var webView: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.apptentive_activity_navigate_to_link)
@@ -20,12 +20,17 @@ internal class NavigateTolinkActivity : BaseNavigateToLinkActivity() {
             finish()
         }
 
-        val webView = findViewById<WebView>(R.id.apptentive_webview_navigate_to_link)
-        val webSettings: WebSettings = webView.settings
-        webSettings.javaScriptEnabled = true
+        webView = findViewById<WebView>(R.id.apptentive_webview_navigate_to_link)
+        webView.settings.javaScriptEnabled = true
         val url = intent.getStringExtra("linkUrl")
-        if (url != null) {
-            webView.loadUrl(url)
-        }
+        if (savedInstanceState != null) {
+            webView.restoreState(savedInstanceState)
+        } else
+            url?.let { webView.loadUrl(it) }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        webView.saveState(outState)
     }
 }
