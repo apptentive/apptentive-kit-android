af56092ebd3dd4a0f635c6629d5035477b16ac8c
Poornima Nagarajan <poornimanagarajan@pnaraj541.hsd1.wa.comcast.net>
2024-07-31 09:43:07 -0700

    prep for public repo udpate

--- app-ui-test/src/androidTest/java/apptentive/com/app/test/helpers/CheckDoesNotExist.kt
+++
@@ -1,9 +0,0 @@
-package apptentive.com.app.test.helpers
-
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
-import androidx.test.espresso.matcher.ViewMatchers.withId
-
-fun checkDoesNotExist(id: Int) {
-    onView(withId(id)).check(doesNotExist())
-}
