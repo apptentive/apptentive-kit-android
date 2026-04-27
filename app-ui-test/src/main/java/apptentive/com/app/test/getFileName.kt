4538e43fd801b1f7a85127c0a392f145affbbaa1
Poornima Nagarajan <poornima.nagarajan@apptentive.com>
2026-04-27 10:21:11 -0700

    public branch changes

--- app-ui-test/src/main/java/apptentive/com/app/test/getFileName.kt
+++
@@ -1,8 +0,0 @@
-package apptentive.com.app.test
-
-import java.io.File
-
-fun getFileName(path: String): String {
-    val pos = path.lastIndexOf(File.pathSeparatorChar)
-    return path.substring(pos + 1)
-}
